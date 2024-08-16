bb87585b7bbfd196b666c96da985a8034b1cd8b3
ipavlakos <pavlakosIlias@outlook.com>
2024-08-16 20:32:07 +0300

    Injecting services into services

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,15 +1,16 @@
 import {Injectable, signal} from "@angular/core";
 
 import {Task, TaskStatus} from "./task.model";
+import {LoggingService} from "../logging.service";
 
-// @Injectable({
-//   providedIn: "root",
-// })
+@Injectable({
+  providedIn: "root",
+})
 export class TasksService {
-
   private tasks = signal<Task[]>([]);
-
   allTasks = this.tasks.asReadonly();
+
+  constructor(private loggingService: LoggingService) {}
 
   addTask(taskData: { title: string; description: string }) {
     const newTask: Task = {
@@ -18,9 +19,13 @@ export class TasksService {
       status: 'OPEN'
     }
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+
+    this.loggingService.log('Added task with id ' + newTask.id);
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
     this.tasks.update((tasks) => tasks.map((task) => task.id === taskId ? {...task, status: newStatus} : task));
+
+    this.loggingService.log('Updated task with id ' + taskId);
   }
 }
